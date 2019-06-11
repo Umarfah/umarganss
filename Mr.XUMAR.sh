99d59acc2699524ffc5c2786a1f57dc8d765316e
umar ganss <50758136+Umarfah@users.noreply.github.com>
2019-06-11 21:57:01 +0800

    umar
    umar

---
+++ Mr.XUMAR.sh
@@ -0,0 +1,163 @@
+#!/bin/sh
+clear
+cy='\033[36;1m' #cyan
+echo
+echo
+pkg update -y
+pkg upgrade
+pkg install toilet -y
+pkg install figlet -y
+pkg install ruby -y
+pkg install git -y
+gem install lolcat
+clear
+toilet -f big -F gay "Tunggu..."
+echo "loading bentar"|lolcat
+sleep 3
+echo "I";
+sleep 0.1;
+clear
+echo "II";
+sleep 0.1;
+clear
+echo "III";
+sleep 0.1;
+clear
+echo "IIII";
+sleep 0.1;
+clear
+echo "IIIII";
+sleep 0.1;
+clear
+echo "IIIIII";
+sleep 0.1;
+clear
+echo "IIIIIII";
+sleep 0.1;
+clear
+echo "IIIIIIII";
+sleep 0.1;
+clear
+echo "IIIIIIIII";
+sleep 0.1;
+clear
+echo "IIIIIIIIII";
+sleep 0.1;
+clear
+echo "I";
+sleep 0.1;
+clear
+echo "II";
+sleep 0.1;
+clear
+echo "III";
+sleep 0.1;
+clear
+echo "IIII";
+sleep 0.1;
+clear
+echo "IIIII";
+sleep 0.1;
+clear
+echo "IIIIII";
+sleep 0.1;
+clear
+echo "IIIIIII";
+sleep 0.1;
+clear
+echo "IIIIIIII";
+sleep 0.1;
+clear
+echo "IIIIIIIII";
+sleep 0.1;
+clear
+echo "IIIIIIIIII";
+sleep 0.1;
+clear
+echo "I";
+sleep 0.1;
+clear
+echo "II";
+sleep 0.1;
+clear
+echo "III";
+sleep 0.1;
+clear
+echo "IIII";
+sleep 0.1;
+clear
+echo "IIIII";
+sleep 0.1;
+clear
+echo "IIIIII";
+sleep 0.1;
+clear
+echo "IIIIIII";
+sleep 0.1;
+clear
+echo "IIIIIIII";
+sleep 0.1;
+clear
+echo "IIIIIIIII";
+sleep 0.1;
+clear
+echo "IIIIIIIIII";
+sleep 0.1;
+clear
+echo  " 1010101010101010101010101010101010101010 "|lolcat
+echo  "1  _     _   ___   ___    ___     ____   1"|lolcat
+echo  "0 I I   I I I   \_/   I  I _ I   I __ I  0"|lolcat
+echo  "1 I I   I I I Il   Il I I I_I I I I__I I 1"|lolcat
+echo  "0 I I   I I I I I_I I I I  _  I I  _   I 0"|lolcat
+echo  "1 I I   I I I I     I I I I I I I I I I  1"|lolcat
+echo  "0  I I_I I  I I     I I I I I I I I  I I 0"|lolcat
+echo  "1   I___I   I_I     I_I I_I I_I I_I  I_I 1"|lolcat
+echo  "0                                        0"|lolcat
+echo  "1 Author : MR.XUMAR 02                   1"|lolcat
+echo  "0 github : https://github.com/umarfah/   0"|lolcat
+echo  "1 team   : FOURSD34TH#T34M               1"|lolcat
+echo  "0 thanks : Allah, all member FSDHT       0"|lolcat
+echo  "1          Mr.13DL                       1"|lolcat
+echo  "0                                        0"|lolcat
+echo  " 1010101010101010101010101010101010101010 "|lolcat
+echo  "------------------------------------------"|lolcat
+echo  "x-X[+] WELCOME TO MYTOOLS INSTALLER [+]X-x"|lolcat
+echo  " ________________________________________ "|lolcat
+echo  "I_____________TOOLS-UTAMAA_______________I"|lolcat
+echo  " A.) Tools Umar"|lolcat
+echo  " ________________________________________ "|lolcat
+echo  "I_____________TOOLS-PNDMPG_______________I"|lolcat
+echo  " B) Tools Mr.13DL"|lolcat
+echo  " ________________________________________ "|lolcat
+echo  "I________________________________________I"|lolcat
+echo $blue
+echo $cy" ›››››››››››[+]Mr.XUMAR[+]‹‹‹‹‹‹‹‹‹‹‹‹‹"
+echo $cy"/"
+read -p"\_# " BDL;
+
+if [ $BDL = A ] || [ $BDL = A ]
+then
+clear
+toilet -f big -F gay "Umartools"
+sleep 3
+pkg update -y
+pkg upgrade
+pkg install python
+pkg install python2
+git clone https://github.com/umarfah/umarganss
+cd umarganss
+php arya.php
+fi
+
+if [ $BDL = B ] || [ $BDL = B ]
+then
+clear
+toilet -f big -F gay "Wadimortools"
+sleep 3
+pkg update -y
+pkg upgrade
+pkg install git
+git clone https://github.com/CHWTMB13/Mr.13DL-Tools-v.0.1
+cd Mr.13DL-Tools-v.0.1
+sh Tools.sh
+fi
